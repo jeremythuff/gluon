ac817eb8f5b07cffb9dd9827bc2a9543c4c137eb
Jeremy Huff <jeremythuff@gmail.com>
2016-11-30 22:49:51 -0600

    Init commit

---
+++ electron-start.js
@@ -0,0 +1,34 @@
+const electron = require("electron");
+
+const {app, BrowserWindow, globalShortcut} = electron;
+
+let mainWindow;
+
+app.setName("Desktop App");
+
+app.on("ready", () => {
+        mainWindow = new BrowserWindow({
+                width: 1280,
+                height: 900,
+                show: false
+        });
+
+        mainWindow.loadURL(`file://${__dirname}/dist/index.html`);
+
+        mainWindow.once('ready-to-show', () => {
+                mainWindow.show()
+        });
+
+        mainWindow.on("closed", () => {
+                mainWindow = null;
+        });
+
+});
+
+app.on("will-quit", () => {
+        globalShortcut.unregisterAll();
+});
+
+app.on("window-all-closed", () => {
+        app.quit();
+});
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'home',
+  styleUrls: ['./home.component.scss'],
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {
+}
