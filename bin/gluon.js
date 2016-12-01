16dfaf2c3400771779c8dd45d03e4e20290751be
Jeremy Huff <jeremythuff@gmail.com>
2016-12-01 16:13:54 -0600

    No launching electron from the gluon cli

--- bin/gluon.js
+++ bin/gluon.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
-require('shelljs/global');
+var nodeCLI = require("shelljs-nodecli");
+var colors = require('colors');
 
-console.log("Test");
+console.log("\n\nGluon running...\n".bold.underline.green);
+console.log("ctrl-c kills process\n".red);
+
+nodeCLI.exec("electron", process.argv[2]);
+
