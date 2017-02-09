3e97a405eeceda5c6af15272223a82cc7877bf85
jeremythuff <jeremythuff@gmail.com>
2017-02-09 16:07:06 -0600

    removed incorrect directory check

--- bld/html.js
+++ bld/html.js
@@ -1,7 +1,7 @@
 require('shelljs/global');
  
 if (!test('-d', "dist")) mkdir("dist");
-if (!test('-d', "dist/app")) mkdir("dist/app");
+if (!test('-d', "dist/launcher")) mkdir("dist/launcher");
 
 cp("src/resources/html/main.html", "dist/launcher");
 cp("-Rf", "src/resources/html/", "dist/launcher");
