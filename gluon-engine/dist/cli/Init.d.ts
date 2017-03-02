7ce09acc15a42c1485719bd0b3e7e45b082d8e2b
jeremythuff <huff@library.tamu.edu>
2017-03-02 00:14:19 -0600

    Class cacing initialized main game class name

--- gluon-engine/dist/cli/Init.d.ts
+++ gluon-engine/dist/cli/Init.d.ts
@@ -3,4 +3,5 @@ import AbstractCliCommand from "./AbstractCliCommand";
 export default class Init extends AbstractCliCommand implements CliCommand {
     static help: [string, string];
     execute(args: Array<string>): void;
+    private classCase(str);
 }
