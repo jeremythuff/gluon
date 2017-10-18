81e86ff15a863bfa82e9c30253dea674277ddfba
Jeremy Huff <jeremy@Huffsiz-MacBook-Air.local>
2017-10-18 00:54:59 -0500

    Changes webpack build

--- config/webpack/engine/webpack.base.js
+++ config/webpack/engine/webpack.base.js
@@ -1,11 +1,52 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require('path');
 
+console.log(__dirname);
 
 module.exports = {
-    entry: './src/engine/index.ts',
+    context: path.join(__dirname, '../../../'),
+    entry: {
+        ai: './modules/ai/index.ts',
+        audio: './modules/audio/index.ts',
+        core: './modules/core/index.ts',
+        networking: './modules/networking/index.ts',
+        physics: './modules/physics/index.ts',
+        ui: './modules/ui/index.ts',
+    },
     output: {
-        filename: './dist/engine/bundle.js'
+        filename: './dist/modules/[name]/bundle/[name].js',
+        library: 'gluon-engine',
+        libraryTarget: 'umd'
     },
     resolve: {
         extensions: ['.ts', '.js'] 
-    }
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { 
+                from: 'modules/ai/package.json',
+                to: 'dist/modules/ai/package.json'
+            },
+            { 
+                from: 'modules/audio/package.json',
+                to: 'dist/modules/audio/package.json'
+            },
+            { 
+                from: 'modules/core/package.json',
+                to: 'dist/modules/core/package.json'
+            },
+            { 
+                from: 'modules/networking/package.json',
+                to: 'dist/modules/networking/package.json'
+            },
+            { 
+                from: 'modules/physics/package.json',
+                to: 'dist/modules/physics/package.json'
+            },
+            { 
+                from: 'modules/ui/package.json',
+                to: 'dist/modules/ui/package.json'
+            }
+        ])
+    ] 
 }
